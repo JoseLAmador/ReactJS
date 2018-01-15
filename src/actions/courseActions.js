3979c16500bdb531039189a8f4a9ca989899b644
JoseLAmador <josel.amador.s@gmail.com>
2018-01-14 23:22:46 -0600

    Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,17 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course}
+export function loadCoursesSucces(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses}
+}
+
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSucces(courses));
+    }).catch(error => {
+      throw (error);
+    });
+  }
 }
